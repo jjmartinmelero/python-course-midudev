4ae00749a489212de22234006ea829b8689da382
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2026-02-19 08:34:06 +0000

    added 01 print and 02 types files

---
+++ 01_basic/01_print.py
@@ -0,0 +1,12 @@
+
+
+print("Hello world !")
+
+print('with single quotes')
+
+print("Python", "is", "great !")
+
+print("Python", "is", "great !", sep="-")
+
+print("Python", "is", "great !", end=" ")
+print("one line")
---
+++ 01_basic/02_types.py
@@ -0,0 +1,34 @@
+
+print("int: ")
+print(10)
+print(0)
+print(-5)
+
+# get the type
+print(type(10))
+
+
+print("Float: ")
+print(type(3.14))
+print(3.14)
+
+
+print("Complex: ")
+print(type(1 + 2j))
+
+print("str: ")
+print(type("Hola"))
+print(type(""))
+print(type("""
+    Multiline
+"""))
+
+
+print("Bool: ")
+print(type(True))
+print(type(False))
+
+
+print("NoneType: ")
+
+print(type(None))
