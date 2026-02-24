9b44accfcea64127ea2ac2c87b883e1c76ea2a4a
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2026-02-24 11:57:10 +0000

    added file 03 list

---
+++ 02_flow_control/03_list.py
@@ -0,0 +1,26 @@
+
+
+# listas
+list1 = [1, 2, 3, 4, 5]
+list2 = ["test1", "test2", "test3", "test4"]
+list3 = [1, "hello", True, 3.4]
+empty_list = []
+list_of_lists = [[1, 2], [3, 4]]
+matrix = [[1, 2], [2, 3], [3, 4]]
+
+print(list1)
+print(list2)
+print(list3)
+print(empty_list)
+print(list_of_lists)
+print(matrix)
+
+
+# get elements
+print("\n get elements by index")
+print(list2[0])
+print(list2[-1])  # test4
+print(list2[-2])  # test3
+
+# access element with matrix
+print(list_of_lists[1][0])
