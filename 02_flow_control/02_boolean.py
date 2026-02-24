9a8979095b8cd871d68861e6934db6a9bb62c360
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2026-02-24 08:12:14 +0000

    added boolean file

---
+++ 02_flow_control/02_boolean.py
@@ -0,0 +1,29 @@
+
+
+print("\n Basic booleans: ")
+
+print(True)
+print(False)
+
+print("\n Comparation operators: ")
+print("5 > 3:", 5 > 3)
+print("5 < 3:", 5 < 3)
+print("5 == 3:", 5 == 3)
+print("5 != 3:", 5 != 3)
+print("5 >= 3:", 5 >= 3)
+print("5 <= 3:", 5 <= 3)
+
+
+print("\n Comparation strings")
+print("manzana" < "pera")  # True
+print("Hola" == "hola")  # False
+
+
+print("\n Logic operators: ")
+
+print(True and True)   # True
+print(True and False)  # False
+print(True or False)   # True
+print(False or False)  # False
+print(not True)        # False
+print(not False)       # True
