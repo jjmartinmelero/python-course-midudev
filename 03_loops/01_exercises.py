234597ec563c78cf0fa56bbe0c0e1c2962b8d23f
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2026-02-26 14:09:27 +0000

    updated exercises 1 and 2

--- 03_loops/01_exercises.py
+++ 03_loops/01_exercises.py
@@ -6,10 +6,25 @@
 # Imprime los números del 10 al 1 usando un bucle while.
 print("\nEjercicio 1:")
 
+counter = 10
+
+while counter > 0:
+    print(counter)
+    counter -= 1
+
 # Ejercicio 2: Suma de números pares (while)
 # Calcula la suma de los números pares entre 1 y 20 (inclusive) usando un bucle while.
 print("\nEjercicio 2:")
 
+counter = 1
+total = 0
+
+while counter <= 20:
+    if counter % 2 == 0:
+        total += counter
+    counter += 1
+else:
+    print(f"total: {total}")
 
 # Ejercicio 3: Factorial de un número
 # Pide al usuario que introduzca un número entero positivo.
@@ -17,6 +32,9 @@ print("\nEjercicio 2:")
 # El factorial de un número entero positivo es el producto de todos los números del 1 al ese número. Por ejemplo, el factorial de 5
 # 5! = 5 x 4 x 3 x 2 x 1 = 120.
 print("\nEjercicio 3:")
+
+
+num_factorial =
 
 
 # Ejercicio 4: Validación de contraseña
