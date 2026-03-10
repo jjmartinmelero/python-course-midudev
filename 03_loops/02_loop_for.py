cbfbf3491f7cf144508250a0656091e119579fcb
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2026-03-10 10:02:52 +0000

    added 02 loop for file

---
+++ 03_loops/02_loop_for.py
@@ -0,0 +1,51 @@
+
+
+print("\n Loop for: ")
+
+fruits = ["apple", "banana", "cherry", "orange",
+          "strawberry", "watermelon", "pineapple", "pear"]
+
+for fruit in fruits:
+    print(fruit)
+
+
+# iterate a string
+string = "juan jesus"
+
+for character in string:
+    print(character)
+
+
+# use enumerate
+for index, fruit in enumerate(fruits):
+    print(f"index: {index} fruit: {fruit}")
+
+
+# nest loops
+letters = ['a', 'b', 'c']
+numbers = [1, 2, 3]
+
+for letter in letters:
+    for number in numbers:
+        print(f"{letter} x {numbers} = {letter * number}")
+
+
+# example with break
+print("\n break:")
+animals = ['dog', 'cat', 'bird']
+
+for animal in animals:
+    if animal == "cat":
+        break
+
+    print(animal)
+
+# example with continue
+print("\n continue:")
+animals = ['dog', 'cat', 'bird']
+
+for animal in animals:
+    if animal == "cat":
+        continue
+
+    print(animal)
