0cddafa7ac834c5424b65bb9646ec8d8e618a066
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2026-03-10 11:33:32 +0000

    added 03 range file

---
+++ 03_loops/03_range.py
@@ -0,0 +1,20 @@
+
+
+print("\n range")
+
+nums = range(5)
+
+for num in nums:
+    print(num)
+
+
+# range with step
+for num in range(0, 10, 2):
+    print(num)
+
+# with negative numbers
+for num in range(-5, 0):
+    print(num)
+
+for num in range(10, 0, -1):
+    print(num)
