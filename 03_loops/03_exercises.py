733b2b5c660e792faa7e1e7ccc10321b24a701db
jjmartinmelero <juanjesusmelero@hotmail.com>
2026-03-11 17:12:16 +0100

    added exercises file

---
+++ 03_loops/03_exercises.py
@@ -0,0 +1,25 @@
+# Ejercicio 1: Imprimir números del 1 al 10
+# Imprime los números del 1 al 10 (inclusive) usando un bucle for y range().
+
+for num in range(1, 11):
+    print(num)
+
+# Ejercicio 2: Imprimir números impares del 1 al 20
+# Imprime todos los números impares entre 1 y 20 (inclusive) usando un bucle for y range().
+for num in range(20):
+    if (num + 1) % 2 != 0:
+        print(num + 1)
+
+
+# Ejercicio 3: Imprimir múltiplos de 5
+# Imprime los múltiplos de 5 desde 5 hasta 50 (inclusive) usando un bucle for y range().
+
+# Ejercicio 4: Imprimir números en orden inverso
+# Imprime los números del 10 al 1 (inclusive) en orden inverso usando un bucle for y range().
+
+# Ejercicio 5: Suma de números en un rango
+# Calcula la suma de los números del 1 al 100 (inclusive) usando un bucle for y range().
+
+# Ejercicio 6: Tabla de multiplicar
+# Pide al usuario que introduzca un número.
+# Imprime su tabla de multiplicar del 1 al 10 usando un bucle for y range().
