c20a49b4dae5ad4d4232e018575bb2f0bceb76a9
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2026-03-12 08:01:21 +0000

    added 04 function file

---
+++ 04_functions/04_functions.py
@@ -0,0 +1,21 @@
+
+
+def greeting():
+    print("Hello !!")
+
+
+greeting()
+
+
+def greeting_to(name):
+    print(f"Hello, {name}")
+
+
+greeting_to("Juan Jesus")
+
+
+def add(a, b):
+    return a + b
+
+
+print(f"add function: {add(10, 5)}")
