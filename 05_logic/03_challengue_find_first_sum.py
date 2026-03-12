904acff6765366e9659a49abe0a13439081a23cd
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2026-03-12 11:48:31 +0000

    updated 03 exercise

---
+++ 05_logic/03_challengue_find_first_sum.py
@@ -0,0 +1,9 @@
+# Objetivo
+# Dado un array de números y un número objetivo (goal), necesitamos encontrar los dos primeros números en el array cuya suma sea igual a ese número objetivo. Si existe tal combinación, la función debe devolver los índices de estos dos números. Si no se encuentra ninguna combinación, debe devolver None.
+
+# example:
+# nums = [4,5,6,2]
+# goal = 8
+# find_first_sum(nums, goal) -> [2,3]
+
+
